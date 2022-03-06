e68600c00f98add1247c77652f80e6cf3b00663e
nger <nger2512@gmail.com>
2022-03-06 20:32:37 +0700

    hell

--- app/routes/backend/index.js
+++ app/routes/backend/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.use('/',require('./home'));
-router.use('/dashboard',require('./dashboard/'));
-router.use('/items',require('./items/'));
+router.use('/dashboard',require('./dashboard'));
+router.use('/items',require('./items'));
 module.exports = router;
